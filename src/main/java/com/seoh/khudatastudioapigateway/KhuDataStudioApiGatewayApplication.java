e35efc9101f7bc6b144688b9be590b0d80f2a96d
seoh02h <seok02h@khu.ac.kr>
2022-10-25 23:53:40 +0900

    Add api gateway

---
+++ src/main/java/com/seoh/khudatastudioapigateway/KhuDataStudioApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.seoh.khudatastudioapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KhuDataStudioApiGatewayApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(KhuDataStudioApiGatewayApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/seoh/khudatastudioapigateway/KhuDataStudioApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seoh.khudatastudioapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KhuDataStudioApiGatewayApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
